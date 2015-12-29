80fa08c68b411da9ac9eae9b7a5f951cd991061b
c.piotre <c.piotre@gmail.com>
2015-12-30 00:28:06 +0100

    Wire up the fresh gradle

--- bash/install-additionals.sh
+++ bash/install-additionals.sh
@@ -1,11 +1,21 @@
 sudo apt-get install git
 sudo apt-get install vim
+
+# Java 
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
 sudo apt-get install oracle-java8-set-default
+
+# Maven
 sudo apt-get install maven
+
+# Gradle 
+sudo add-apt-repository ppa:cwchien/gradle
+sudo apt-get update
 sudo apt-get install gradle
+
+# Films
 sudo apt-get install vlc 
 sudo apt-get install qnapi
 
