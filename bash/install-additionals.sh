e9bf955fe432af878b2db77229191a170c683900
c.piotre <c.piotre@gmail.com>
2015-12-29 21:32:01 +0100

    Add installation script for applications added on vanilla system

---
+++ bash/install-additionals.sh
@@ -0,0 +1,11 @@
+sudo apt-get install git
+sudo apt-get install vim
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
+sudo apt-get install maven
+sudo apt-get install gradle
+sudo apt-get install vlc 
+sudo apt-get install qnapi
+
