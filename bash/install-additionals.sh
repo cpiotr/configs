0ad40e4e34f6a921b05295bf7d15e7cdfe3f586c
c.piotre <c.piotre@gmail.com>
2015-12-30 23:16:45 +0100

    Update Maven version

--- bash/install-additionals.sh
+++ bash/install-additionals.sh
@@ -8,7 +8,10 @@ sudo apt-get install oracle-java8-installer
 sudo apt-get install oracle-java8-set-default
 
 # Maven
-sudo apt-get install maven
+sudo apt-get purge maven maven2 maven3
+sudo apt-add-repository ppa:andrei-pozolotin/maven3
+sudo apt-get update
+sudo apt-get install maven3
 
 # Gradle 
 sudo add-apt-repository ppa:cwchien/gradle
