bc093c7582c5fe245d7ba3fa344c9a1a80d9c7f4
c.piotre <c.piotre@gmail.com>
2016-06-28 22:58:19 +0200

    Add scala installation script

---
+++ bash/scala.sh
@@ -0,0 +1,10 @@
+sudo apt-get remove scala-library scala
+sudo wget www.scala-lang.org/files/archive/scala-2.10.4.deb
+sudo dpkg -i scala-2.11.5.deb
+sudo apt-get update
+sudo apt-get install scala
+wget http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.13.0/sbt.deb
+sudo dpkg -i sbt.deb
+sudo apt-get update
+sudo apt-get install sbt
+
