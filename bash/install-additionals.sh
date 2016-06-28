c89ec68ef53eec12dee17f0cb8520dcf26cc8e6a
c.piotre <c.piotre@gmail.com>
2016-06-28 22:57:35 +0200

    Add docker and zsh

--- bash/install-additionals.sh
+++ bash/install-additionals.sh
@@ -25,3 +25,14 @@ sudo apt-get install qnapi
 # Books
 sudo apt-get install calibre
 
+# Docker
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo sh -c 'echo "\ndeb https://apt.dockerproject.org/repo ubuntu-trusty main\n" >> /etc/apt/sources.list.d/docker.list'sudo apt-get update
+sudo apt-get install linux-image-extra-$(uname -r)
+sudo apt-get autoremove
+sudo apt-get install docker-engine
+
+sudo apt-get install htop
+sudo apt-get install zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
