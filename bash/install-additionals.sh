906ae6535f9cabb998d067e2bb3707cbfda72893
cpiotr <c.piotre@gmail.com>
2017-03-29 13:21:30 +0200

    Update additionals with tmux

--- bash/install-additionals.sh
+++ bash/install-additionals.sh
@@ -36,3 +36,4 @@ sudo apt-get install htop
 sudo apt-get install zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+sudo apt-get install tmux
